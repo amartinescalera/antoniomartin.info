2e0240503f23dfad2a785752b4a1be70dc7e7533
Antonio Martin <antoniomeh@gmail.com>
2015-06-13 17:17:14 +0100

    Improve the code and the information

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 
 $EmailFrom = $_REQUEST['email']; 
-$EmailTo = "antoniomeh@gmail.com"; // Your email address here
+$EmailTo = "info@antoniomeh.info"; // Your email address here
 $Subject = "Contact form";
 $Name = Trim(stripslashes($_POST['name'])); 
 $Email = Trim(stripslashes($_POST['email'])); 
